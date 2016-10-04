a4af33a183e5484a2aae3dd9ad47c188caa5f7a4
BIGStrawberry <wouter.dijkstra.wd@gmail.com>
2016-10-04 23:06:16 +0200

    Added database config details

--- Utils/config.py
+++ Utils/config.py
@@ -1,2 +1,6 @@
 RDW_KEY = '8c33d83d7ccc46af1fe7b30e8e019a7986bbd2d0379d893f19a320e263e34607'
 RDW_URL = 'https://overheid.io/api'
+DB_HOST = 'vps306311.ovh.net'
+DB_USER = 'nonroot'
+DB_PASS = 'pass'
+DB = 'server'
