8885c44f3dca7f9ddaca095ce5ba3026b029bcbb
BIGduzy <nick-b@live.nl>
2016-10-04 22:47:18 +0200

    Code standard and cleanup

--- RDW/main.py
+++ RDW/main.py
@@ -3,13 +3,11 @@ from RDW.rdwClient import RdwClient
 
 client = RdwClient()
 # TODO: get licence plate from Cammile
-data = client.fetch_by_plate('4-TFL-24')
-if data:
-    print(data)
-
-    if client.validate_plate(data):
-        # TODO: Connect this with Wouter's database
-        print('Valid car')
-    else:
-        # TODO: connect this with Marco's GUI
-        print('Wouter komt je stompen')
+# fetch the data
+client.fetch_by_plate('4-TFL-24')
+if client.validate_plate():
+    # TODO: Connect this with Wouter's database
+    print('Valid car')
+else:
+    # TODO: connect this with Marco's GUI
+    print('Wouter komt je stompen')
