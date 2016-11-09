a55732050f42a05b7f6207630a99817a4cb5330e
BIGduzy <nick-b@live.nl>
2016-11-09 16:42:59 +0100

    Kappa

--- Utils/validatePlate.py
+++ Utils/validatePlate.py
@@ -1,11 +1,4 @@
-def validate_plate(data) -> bool:
-    if not data:
-        return False
-
-    date = data['datumeersteafgiftenederland']
-    year = date.split("-")
-    fuel_type = data['hoofdbrandstof']
-
+def validate_plate(fuel_type, year) -> bool:
     if fuel_type.lower() == 'diesel' and int(year[0]) < int('2001'):
         return False
     return True
